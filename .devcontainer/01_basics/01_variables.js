5622dd8945377ca28f14ca7493d5237dcd148fa2
oggy800 <mrahulkumar991@gmail.com>
2025-07-01 07:30:00 +0000

    contaring coding done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountpassword = "12345"
+accountcity = "bihar"
+
+// accountId = 2 //not allowed
+
+accountEmail = "hc@hc.com"
+accountpassword = "58465"
+accountcity = "delhi"
+
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountpassword, accountcity])
+
