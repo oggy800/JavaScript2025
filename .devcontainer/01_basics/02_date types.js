77c695644b3fee8ae766d90a76e7a7fa181ae36c
oggy800 <mrahulkumar991@gmail.com>
2025-07-02 15:10:40 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_date types.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer varsion
+
+// alert(3+3) // we are using nodejs, not browser
+
+ console.log(3
+        +
+        3) //code readability should be high
+
+console.log("rahu")
+
+
+let name = "rahul"
+let age = 19
+let isloggrdin = false
+
+// number => 2 to power 53 
+// bigint 
+//string =>""
+//boolean => true/false
+//null => standalone value
+// undefined =>
+// symbol => uniqui
+
+
+// object
+
+console.log(typeof underfined); // underfined
+console.log(typeof null); // object
