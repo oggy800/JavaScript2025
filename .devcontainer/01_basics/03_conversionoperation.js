4e43e12f4c6d45c918e6db795f5e1c0deba6fbc7
oggy800 <mrahulkumar991@gmail.com>
2025-07-02 15:37:46 +0000

    conversiono data types

---
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -0,0 +1,28 @@
+let score = null
+
+console. log(typeof score);
+console. log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33 ye numbar hihota ha 
+// "33ahdg" => nan = not a numbar
+// "true" => 1; false => 0
+
+let isloggrdin = "rahul"
+
+let booleanisloggedin = Boolean(isloggedin)
+
+console.log(booleanisloggedin);
+
+// 1 => true; 0 => false
+// "" => false
+//"rahul" => true
+
+let somenumbar = 
+
+let stringnumbar = string (somenumbar)
+console.log(stringnumbar);
+console.log(typeof stringnumbar);
