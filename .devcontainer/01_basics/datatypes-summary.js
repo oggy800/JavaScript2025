ed492f91b6a033dc0e1f050a72d0617c4b02d6b7
oggy800 <mrahulkumar991@gmail.com>
2025-07-03 09:11:26 +0000

    datatypes-summary

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+// primitive 
+
+// 7 types : string, numbar, boolearn, null, undefined, symbol, bigint
+
+const score = 100
+const scorevalue = 100.3
+
+const isloggrdin = false
+const outsidetemp = null
+let useremail;
+
+const id = symbol('123')
+const anotherid = symbol('123')
+
+console.log(id===anotherid)
+
+const bignumbar = 64653653151n
+
+
+
+// reference (non primitive)
+
+// array, objects, functions
+
+const heros = ["shaktiman, naagraj, doga"];
+let myobj = {
+    name: "rahul",
+    age: 19,
+
+}
+
+const myfunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof bignumbar);
